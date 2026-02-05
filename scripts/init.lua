f15d760ee591d6832e08b9e033e75c6b1f78c501
maYayoh <mayayoh@dotslashfade.dev>
2026-02-05 23:38:45 +0100

    Cleaning up and fixes

--- scripts/init.lua
+++ scripts/init.lua
@@ -9,3 +9,20 @@ ScriptHost:LoadScript("scripts/locations.lua")
 Tracker:AddLayouts("layouts/items.json")
 Tracker:AddLayouts("layouts/tracker.json")
 Tracker:AddLayouts("layouts/broadcast.json")
+
+-- Update total cubes with Golden and Anti counts
+local function updateTotal()
+    local g = Tracker:FindObjectForCode("golden")
+    local a = Tracker:FindObjectForCode("anti")
+    local t = Tracker:FindObjectForCode("total")
+
+    if not g or not a or not t then
+        return
+    end
+
+    t.AcquiredCount = g.AcquiredCount + a.AcquiredCount
+end
+
+ScriptHost:AddWatchForCode("WatchGolden", "golden", updateTotal)
+ScriptHost:AddWatchForCode("WatchAnti", "anti", updateTotal)
+ScriptHost:AddWatchForCode("PreventTotal", "total", updateTotal)
